0b6addd17006fad66e9842a9f89ee5af6629ea0d
maahiali <maahiali222@gmail.com>
2020-07-29 22:26:46 +0530

    going to fight for conflict

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class helloworld
 {
 public static void main(string[] args)
 {
-system.out.println("hello maahi");
-system.out.println("hii maahi");
+system.out.println("hello maahiali");
+system.out.println("hii222 to maahi");
 }
 }
