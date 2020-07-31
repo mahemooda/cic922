cd4ad5a787a864ee1e1cdffc5fa407b27da627f4
maahiali <maahiali222@gmail.com>
2020-07-31 08:30:39 +0530

    gitignoredemo

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class helloworld
 {
 public static void main(string[] args)
 {
-system.out.println("this is my master branch");
+system.out.println("this is my working location");
 system.out.println("this is my new implemented in develope branch");
 system.out.println("this is my new implemented in feature branch");
 }
